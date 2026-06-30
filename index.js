70008b2b46862e4d529dfcbc96b9faf548967f4d
keshavsoft <nknnkumar@yahoo.com>
2026-06-30 16:20:45 +0530

    Initial git sync

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ import { Client } from "ssh2";
 const ssh = new Client();
 
 ssh.on("ready", () => {
-    console.log("Connected to server.");
-    ssh.end();
+    console.log("Connected.");
+
+    ssh.exec(`cd ${process.env.SSH_REPO_PATH} && git pull`,
+        (err, stream) => {
+            if (err) throw err;
+
+            stream.on("data", (data) => {
+                console.log(data.toString());
+            });
+
+            stream.stderr.on("data", (data) => {
+                console.error(data.toString());
+            });
+
+            stream.on("close", () => {
+                ssh.end();
+            });
+        });
 });
 
-ssh.on("error", (err) => {
-    console.error(err);
-});
+ssh.on("error", console.error);
 
 ssh.connect({
     host: process.env.SSH_HOST,
